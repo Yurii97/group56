d79d81e77f33a86a5f318d35f060fc4243ba38b4
Yurii Onats <yura.onats@gmail.com>
2021-10-10 17:39:30 +0300

    222

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './sass/main.scss';
+
+const canvas = document.querySelector('#draw');
+
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+ctx.strokeStyle = '#bada55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+
+let isDrawing = false;
+
+
+
+function draw(ev) {
+    if (!isDrawing) return;
+    console.log(ev);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+})
+canvas.addEventListener('mouseup', () => (isDrawing = false))
+canvas.addEventListener('mouseout', () => (isDrawing = false))
