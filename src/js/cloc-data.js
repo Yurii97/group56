a336fec7890d3e63aa36c489079552713dad25d3
Yurii Onats <yura.onats@gmail.com>
2021-11-07 17:38:12 +0200

    fix Clooc

---
+++ src/js/cloc-data.js
@@ -0,0 +1,13 @@
+
+import { format, compareAsc } from 'date-fns'
+
+const dataDay = document.querySelector('.date__day')
+const mounth = document.querySelector('.date__month')
+const timeRf = document.querySelector('.date__time')
+// canst dayName = document.querySelector('')
+console.log(format(new Date(), 'do'));
+dataDay.textContent = format(new Date(), 'do EEE');
+mounth.textContent = format(new Date(), 'LLLL');
+setInterval(() => {    
+    timeRf.textContent=format(new Date(), 'kk:mm:ss');
+}, 1000);
